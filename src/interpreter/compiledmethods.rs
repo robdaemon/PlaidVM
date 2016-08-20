c07efa0d3efbd88ef4840ac31c97839e712fe22c
Robert Roland <robert@robertroland.org>
2016-08-20 11:57:28 -0700

    Implementing parts of CompiledMethod

--- src/interpreter/compiledmethods.rs
+++ src/interpreter/compiledmethods.rs
@@ -35,30 +35,66 @@ impl CompiledMethod {
     }
 
     pub fn temporary_count_of(&self, method: vm::MethodPointer) -> u16 {
-        0
+        extract_bits(3, 7, self.header_of(method))
     }
 
     pub fn large_context_flag_of(&self, method: vm::MethodPointer) -> u16 {
-        0
+        extract_bits(8, 8, method)
     }
 
     pub fn literal_count_of(&self, method: vm::MethodPointer) -> u16 {
-        0
+        self.literal_count_of_header(self.header_of(method))
     }
 
     pub fn literal_count_of_header(&self, header: u16) -> u16 {
-        0
+        extract_bits(9, 14, header)
     }
 
     pub fn object_pointer_count_of(&self, method_pointer: u16) -> u16 {
-        0
+        self.literal_count_of(method_pointer) + self.literal_start
     }
 
     pub fn initial_instruction_pointer_of_method(&self, method_pointer: u16) -> u16 {
-        0
+        (self.literal_count_of(method_pointer) + self.literal_start)) * 2 + 1
     }
 
     pub fn flag_value_of(&self, method_pointer: u16) -> u16 {
-        0
+        extract_bits(0, 2, self.header_of(method_pointer))
     }
+
+    pub fn field_index_of(&self, method_pointer: u16) -> u16 {
+        extract_bits(3, 7, self.header_of(method_pointer))
+    }
+
+    pub fn header_extension_of(&self, method_pointer: u16) -> u16 {
+        let literal_count = self.literal_count_of(method_pointer);
+
+        self.literal(literal_count - 2, method_pointer)
+    }
+
+    pub fn argument_count_of(&self, method_pointer: u16) -> u16 {
+        let flag_value = self.flag_value_of(method_pointer);
+
+        if flag_value < 5 {
+            flag_value
+        } else if flag_value < 7 {
+            10
+        } else {
+            extract_bits(2, 6, header_extension_of(method_pointer))
+        }
+    }
+
+    pub fn primitive_index_of(&self, method_pointer: u16) -> u16 {
+        let flag_value = self.flag_value_of(method_pointer);
+
+        if flag_value == 7 {
+            extract_bits(7, 14, method_pointer)
+        } else {
+            10
+        }
+    }
+}
+
+pub fn extract_bits(from: u16, to: u16, method_pointer: u16) -> u16 {
+    0
 }
